ab5d5c73b5b361c453f1b64546e3fdb4ca4dad9e
Cody Frank <cody.frank30@gmail.com>
2019-03-23 22:08:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 katz_deli = []
-
+:other_deli = ["Logan", "Avi", "Spencer"]
 def line(array)
   if array == []
     puts "The line is currently empty."
@@ -11,3 +11,4 @@ def line(array)
     end
   end
 end
+line(:other_deli)
