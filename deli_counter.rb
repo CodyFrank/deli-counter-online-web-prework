1a9533d989c642edc74b723d1dfabc9147b90384
Cody Frank <cody.frank30@gmail.com>
2019-03-23 23:19:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(array, name)
   array << name
-  position = name.index
+  position = array.index(name)
   puts "Welcome, #{name}. You are number #{position + 1} in line."
 end
