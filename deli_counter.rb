437e0420fc5302e7fba346acfd18f743f2b14a96
Cody Frank <cody.frank30@gmail.com>
2019-03-23 21:54:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array == []
+    puts "The line is currently empty."
+  else
+    #phrase = ["The line is currently: ", ]
+    array.each_with_index do |name, index|
+      puts " #{index}. #{name}"
+    end
+  end
+end
